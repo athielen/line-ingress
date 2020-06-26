f0324690a8020faed197aefc1c3b759666519c2b
Anthony Thielen <anthony@anthonythielen.com>
2020-06-26 08:37:20 -0500

    added ip address parsing and user agent parsing

--- models/line.go
+++ models/line.go
@@ -3,18 +3,21 @@ package models
 import (
 	"encoding/json"
 	"fmt"
+	uaParser "github.com/mileusna/useragent"
 	log "github.com/sirupsen/logrus"
+	"net"
 	"net/http"
 	"net/url"
+	"strings"
 	"time"
 )
 
 // Line corresponds to an incoming tracking request and contains the data
 // to be persisted.
 type Line struct {
-	Payload []byte
-	JSON string
-	Time time.Time
+	Payload    []byte
+	JSON       string
+	Time       time.Time
 	SourceType string
 }
 
@@ -29,7 +32,7 @@ func (line Line) String() string {
 
 func MarshalRequestJSON(request *http.Request) ([]byte, error) {
 
-	var queryParams, err = url.ParseQuery(request.URL.RawQuery)
+	queryParams, err := url.ParseQuery(request.URL.RawQuery)
 
 	// add error message and prometheus counter
 	if err != nil {
@@ -37,22 +40,84 @@ func MarshalRequestJSON(request *http.Request) ([]byte, error) {
 		queryParams = make(url.Values)
 	}
 
+	ipAddress, err := getIP(request)
+	if err != nil {
+		log.Warn(err.Error())
+	}
+
+	ua, dt := parseUserAgent(request)
+
+	fmt.Println(request)
+
 	return json.Marshal(
-		WebEventFeatures {
-			Method:          request.Method,
-			URL:             request.URL,
-			Host:            request.Host,
-			Form:            request.Form,
-			RemoteAddr:      request.RemoteAddr,
-			IpAddress:	     request.Header.Get("X-FORWARDED-FOR"),
-			RequestURI:      request.RequestURI,
-			QueryParams:     queryParams,
-			Origin:          request.Header.Get("Origin"),
-			Browser:         request.UserAgent(),
-			OperatingSystem: "",
-			DeviceType:      "",
-			VisitorCountry:  "",
-			InitialReferrer: request.Referer(),
+		WebEventFeatures{
+			Method:                 request.Method,
+			URL:                    request.URL,
+			Host:                   request.Host,
+			Form:                   request.Form,
+			RemoteAddr:             request.RemoteAddr,
+			IpAddress:              ipAddress,
+			RequestURI:             request.RequestURI,
+			QueryParams:            queryParams,
+			Origin:                 request.Header.Get("Origin"),
+			Browser:                ua.Name,
+			BrowserVersion:         ua.Version,
+			OperatingSystem:        ua.OS,
+			OperatingSystemVersion: ua.OSVersion,
+			DeviceType:             dt,
+			Device:                 ua.Device,
+			VisitorCountry:         "",
+			InitialReferrer:        request.Referer(),
 		})
 
+}
+
+func parseUserAgent(r *http.Request) (uaParser.UserAgent, string) {
+
+	ua := uaParser.Parse(r.UserAgent())
+
+	var deviceType = ""
+
+	if ua.Mobile {
+		deviceType = "mobile"
+	} else if ua.Tablet {
+		deviceType = "tablet"
+	} else if ua.Desktop {
+		deviceType = "desktop"
+	} else if ua.Bot {
+		deviceType = "bot"
+	}
+
+	return ua, deviceType
+}
+
+func getIP(r *http.Request) (string, error) {
+
+	//Get IP from the X-REAL-IP header
+	ip := r.Header.Get("X-REAL-IP")
+	netIP := net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+
+	//Get IP from X-FORWARDED-FOR header
+	ips := r.Header.Get("X-FORWARDED-FOR")
+	splitIps := strings.Split(ips, ",")
+	for _, ip := range splitIps {
+		netIP := net.ParseIP(ip)
+		if netIP != nil {
+			return ip, nil
+		}
+	}
+
+	//Get IP from RemoteAddr
+	ip, _, err := net.SplitHostPort(r.RemoteAddr)
+	if err != nil {
+		return "", err
+	}
+	netIP = net.ParseIP(ip)
+	if netIP != nil {
+		return ip, nil
+	}
+	return "", fmt.Errorf("No valid ip found")
 }
--- models/web-event-features.go
+++ models/web-event-features.go
@@ -3,18 +3,21 @@ package models
 import "net/url"
 
 type WebEventFeatures struct {
-	Method          string
-	URL             *url.URL
-	Host            string
-	Form            url.Values
-	RemoteAddr      string
-	IpAddress		string
-	RequestURI      string
-	QueryParams     url.Values
-	Origin          string
-	Browser         string
-	OperatingSystem string
-	DeviceType      string
-	VisitorCountry  string
-	InitialReferrer string
+	Method                 string
+	URL                    *url.URL
+	Host                   string
+	Form                   url.Values
+	RemoteAddr             string
+	IpAddress              string
+	RequestURI             string
+	QueryParams            url.Values
+	Origin                 string
+	Browser                string
+	BrowserVersion         string
+	OperatingSystem        string
+	OperatingSystemVersion string
+	DeviceType             string
+	Device                 string
+	VisitorCountry         string
+	InitialReferrer        string
 }
